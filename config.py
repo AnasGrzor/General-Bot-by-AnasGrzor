26ad9db59910874e00ec0c94840fefe0daad4f79
AnasGrzor <103470289+AnasGrzor@users.noreply.github.com>
2023-08-19 02:05:20 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
-token = "MTEzMTYwNTkyMjY0OTc1NTczOA.GmSt3m.96FnOn9bdsJURV4J7HR3sZEq7fO856JCzCmxt0"
+token = "Your TOKEN HERE"
 
-DB_Token = "postgres://xffhlyog:Tv04_mNazOJS6tyruPgyZKFjW10Qr-FB@lallah.db.elephantsql.com/xffhlyog"
+DB_Token = "Enter DB TOKEN HERE"
+
+# ElephantSQL was used for this project
